4cffb339c150296c5fd0618541b3dcfafc908c81
RoKerjea <kerjeanr@gmail.com>
2024-11-06 17:29:09 +0100

    feat: can detect color from input, and modify corresponding pixel in output

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,30 @@
+use image::Pixel;
 use rand::Rng;
 
 fn main() {
-	let imgx = 100;
-	let imgy = 200;
+	let imgx = 80;
+	let imgy = 640;
 
 	let mut imgbuf1 = image::ImageBuffer::new(imgx, imgy);
 	let mut rng = rand::thread_rng();
-	for (x, y, pixel) in imgbuf1.enumerate_pixels_mut() {
+	for (_x, _y, pixel) in imgbuf1.enumerate_pixels_mut() {
 		let color = rng.gen_range(0..=1);
 			*pixel = image::Rgb([255 * color, 255* color, 255* color]);
 
 	}
-	let mut finalimg = image::ImageBuffer::new(200, 200);
-	for (x, y, pixel) in imgbuf1.enumerate_pixels_mut() {
-		finalimg.put_pixel(x, y, *pixel);
-		finalimg.put_pixel(x+100, y, *pixel);
-	}
-	for x in 120..175 {
-		for y in 25..29 {
-			let pixel = imgbuf1.get_pixel(x+2-100, y);
-			finalimg.put_pixel(x, y, *pixel);
+	let mut finalimg = image::ImageBuffer::new(640, 640);
+	for stripe in 0..8 {
+		for (x, y, pixel) in imgbuf1.enumerate_pixels_mut() {
+			finalimg.put_pixel(x+(80*stripe), y, *pixel);
 		}
 	}
-	for x in 120..175 {
-		for y in 175..179 {
-			let pixel = imgbuf1.get_pixel(x+2-100, y);
-			finalimg.put_pixel(x, y, *pixel);
-		}
-	}
-	for x in 120..124 {
-		for y in 29..175 {
-			let pixel = imgbuf1.get_pixel(x+2-100, y);
-			finalimg.put_pixel(x, y, *pixel);
-		}
-	}
-	for x in 171..175 {
-		for y in 29..175 {
-			let pixel = imgbuf1.get_pixel(x+2-100, y);
-			finalimg.put_pixel(x, y, *pixel);
+	let input = image::open("42_Logo.svg.png").unwrap();
+	for (x, y, pixel) in input.to_rgba8().enumerate_pixels() {
+		if *pixel == image::Rgba([0, 0, 0, 255]) {
+			finalimg.put_pixel(x+80, y+80, image::Rgb([255, 255, 255]));
 		}
 	}
 	// imgbuf.save("first_test.png").unwrap();
-	image::save_buffer("image.png", &imgbuf1, 100, 200, image::ExtendedColorType::Rgb8).unwrap();
-	image::save_buffer("image2.png", &finalimg, 200, 200, image::ExtendedColorType::Rgb8).unwrap()
+	// image::save_buffer("image.png", &imgbuf1, 80, 200, image::ExtendedColorType::Rgb8).unwrap();
+	image::save_buffer("image2.png", &finalimg, 640, 640, image::ExtendedColorType::Rgb8).unwrap()
 }
