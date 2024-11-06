c9f5ee92d05e9962f7c9c82521f4f990829cd9a7
RoKerjea <kerjeanr@gmail.com>
2024-11-06 15:42:58 +0100

    first working version: a rectangle border deeper than surface!!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 
 fn main() {
-	let imgx = 200;
+	let imgx = 100;
 	let imgy = 200;
 
 	let mut imgbuf1 = image::ImageBuffer::new(imgx, imgy);
@@ -11,36 +11,36 @@ fn main() {
 			*pixel = image::Rgb([255 * color, 255* color, 255* color]);
 
 	}
-	let mut finalimg = image::ImageBuffer::new(400, 200);
+	let mut finalimg = image::ImageBuffer::new(200, 200);
 	for (x, y, pixel) in imgbuf1.enumerate_pixels_mut() {
 		finalimg.put_pixel(x, y, *pixel);
-		finalimg.put_pixel(x+200, y, *pixel);
+		finalimg.put_pixel(x+100, y, *pixel);
 	}
-	for x in 275..325 {
-		for y in 75..79 {
-			let pixel = imgbuf1.get_pixel(x-4-200, y);
-			finalimg.put_pixel(x, y, image::Rgb([255 , 255, 255]));
+	for x in 120..175 {
+		for y in 25..29 {
+			let pixel = imgbuf1.get_pixel(x-2-100, y);
+			finalimg.put_pixel(x, y, *pixel);
 		}
 	}
-	for x in 275..325 {
-		for y in 125..129 {
-			let pixel = imgbuf1.get_pixel(x-4-200, y);
-			finalimg.put_pixel(x, y, image::Rgb([255 , 255, 255]));
+	for x in 120..175 {
+		for y in 175..179 {
+			let pixel = imgbuf1.get_pixel(x-2-100, y);
+			finalimg.put_pixel(x, y, *pixel);
 		}
 	}
-	for x in 275..279 {
-		for y in 79..125 {
-			let pixel = imgbuf1.get_pixel(x-4-200, y);
-			finalimg.put_pixel(x, y, image::Rgb([255 , 255, 255]));
+	for x in 120..124 {
+		for y in 29..175 {
+			let pixel = imgbuf1.get_pixel(x-2-100, y);
+			finalimg.put_pixel(x, y, *pixel);
 		}
 	}
-	for x in 321..325 {
-		for y in 79..125 {
-			let pixel = imgbuf1.get_pixel(x-4-200, y);
-			finalimg.put_pixel(x, y, image::Rgb([255 , 255, 255]));
+	for x in 171..175 {
+		for y in 29..175 {
+			let pixel = imgbuf1.get_pixel(x-2-100, y);
+			finalimg.put_pixel(x, y, *pixel);
 		}
 	}
 	// imgbuf.save("first_test.png").unwrap();
-	image::save_buffer("image.png", &imgbuf1, 200, 200, image::ExtendedColorType::Rgb8).unwrap();
-	image::save_buffer("image2.png", &finalimg, 400, 200, image::ExtendedColorType::Rgb8).unwrap()
+	image::save_buffer("image.png", &imgbuf1, 100, 200, image::ExtendedColorType::Rgb8).unwrap();
+	image::save_buffer("image2.png", &finalimg, 200, 200, image::ExtendedColorType::Rgb8).unwrap()
 }
