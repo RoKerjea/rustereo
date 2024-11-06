1fba7dd5b41d5d2dac7f6aad4a335a594ea3bdfe
RoKerjea <kerjeanr@gmail.com>
2024-11-06 18:05:12 +0100

    problem : shape is broken, but still visible

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::Pixel;
+use image::{GenericImageView, Pixel};
 use rand::Rng;
 
 fn main() {
@@ -13,17 +13,33 @@ fn main() {
 
 	}
 	let mut finalimg = image::ImageBuffer::new(640, 640);
-	for stripe in 0..8 {
-		for (x, y, pixel) in imgbuf1.enumerate_pixels_mut() {
-			finalimg.put_pixel(x+(80*stripe), y, *pixel);
-		}
-	}
 	let input = image::open("42_Logo.svg.png").unwrap();
-	for (x, y, pixel) in input.to_rgba8().enumerate_pixels() {
-		if *pixel == image::Rgba([0, 0, 0, 255]) {
-			finalimg.put_pixel(x+80, y+80, image::Rgb([255, 255, 255]));
+
+	for (x, y, pixel) in imgbuf1.enumerate_pixels_mut() {
+		finalimg.put_pixel(x, y, *pixel);
+	}
+	for stripe in 1..8 {
+		for x in 0..80 {
+			for y in 0..640 {
+				let mut pixel = finalimg.get_pixel(x+(80*(stripe-1)), y);
+				if y > 80 && y < 560 && (x+(80*(stripe-1)) < 480)
+				{
+					let inputpixel = input.get_pixel(x+(80*(stripe-1)), y-80);
+					if inputpixel == image::Rgba([0, 0, 0, 255]) {
+						pixel = finalimg.get_pixel(x+4+(80*(stripe-1)), y);
+						// pixel = &image::Rgb([255, 255, 255]);
+					}
+				}
+				finalimg.put_pixel(x+(80*stripe), y, *pixel);
+			}
 		}
 	}
+	// for (x, y, pixel) in input.to_rgba8().enumerate_pixels() {
+	// 	if *pixel == image::Rgba([0, 0, 0, 255]) {
+	// 		finalimg.put_pixel(x+80, y+80, image::Rgb([255, 255, 255]));
+	// 	}
+	// }
+
 	// imgbuf.save("first_test.png").unwrap();
 	// image::save_buffer("image.png", &imgbuf1, 80, 200, image::ExtendedColorType::Rgb8).unwrap();
 	image::save_buffer("image2.png", &finalimg, 640, 640, image::ExtendedColorType::Rgb8).unwrap()
