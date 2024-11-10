6b4b863b2a8b968835b59e62a313973ce3e073e0
RoKerjea <kerjeanr@gmail.com>
2024-11-10 17:25:01 +0100

    new stero version with a simple colored pattern as a background

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::{GenericImageView, Pixel, RgbaImage, imageops};
+use image::{Pixel, RgbaImage, imageops};
 use rand::Rng;
 use imageproc::{drawing::{self, draw_line_segment_mut, Blend, Canvas}, rect::Rect};
 /*
@@ -184,47 +184,112 @@ where
     }
 }
 
-fn main() {
-	let imgx = 640;
-	let imgy = 640;
+// fn main() {
+// 	let imgx = 640;
+// 	let imgy = 640;
 
-	let mut imgbuf1 = image::ImageBuffer::new(imgx, imgy);
-	let mut rng = rand::thread_rng();
-	for (_x, _y, pixel) in imgbuf1.enumerate_pixels_mut() {
-		let color = rng.gen_range(0..=2);
-		let ratio = ((0.25 * _y as f32) *0.75) as u8;
-		let ratio2 = _y as f32 / (640.0 / 2.0);
-		match color {
-			0 => *pixel = image::Rgba([0, 0, 200-ratio, 127]),
-			1 => *pixel = image::Rgba([255 - ratio, 165 - ratio, 0, 127]),
-			2 => *pixel = image::Rgba([255 - ratio, 165 - ratio, 0, 127]),
-			_ => *pixel = image::Rgba([0, 0, 0, 255]),
-		}
-			// *pixel = image::Rgb([255 * color, 255* color, 255* color]);
+// 	let mut imgbuf1 = image::ImageBuffer::new(imgx, imgy);
+// 	let mut rng = rand::thread_rng();
+// 	for (_x, _y, pixel) in imgbuf1.enumerate_pixels_mut() {
+// 		let color = rng.gen_range(0..=2);
+// 		let ratio = ((0.25 * _y as f32) *0.75) as u8;
+// 		let ratio2 = _y as f32 / (640.0 / 2.0);
+// 		match color {
+// 			0 => *pixel = image::Rgba([0, 0, 200-ratio, 127]),
+// 			1 => *pixel = image::Rgba([255 - ratio, 165 - ratio, 0, 127]),
+// 			2 => *pixel = image::Rgba([255 - ratio, 165 - ratio, 0, 127]),
+// 			_ => *pixel = image::Rgba([0, 0, 0, 255]),
+// 		}
+// 			// *pixel = image::Rgb([255 * color, 255* color, 255* color]);
 
-	}
+// 	}
 	
-	let pixel = image::Rgba([255, 255, 255, 255]);
-	let mut canvas = Blend(RgbaImage::from_pixel(640, 640, pixel));// can be used to genrate a new canvas with a single color
-	// let mut canvas1 = image::ImageBuffer::from_pixel(640, 640, pixel);
-	// let mut canvas = Blend(canvas1);
-	let red = image::Rgba([255, 0, 0, 255]);
-	let blue = image::Rgba([0, 0, 255, 255]);
-	let black = image::Rgba([0, 0, 0, 255]);
-	let grey = image::Rgba([128, 128, 128, 127]);
-	// draw_rectangle(&mut canvas, 320, 320, 100, red);
-	// draw_circle(&mut canvas, 320, 320, 50, blue);
-	// let mut canvas = image::ImageBuffer::new(640, 640);
-	// let color = image::Rgb([255, 0, 0]);
-	// let blue = image::Rgb([0, 0, 255]);
-	let blue2 = image::Rgba([0, 0, 255, 127]);
-	drawing::draw_filled_rect_mut(&mut canvas, Rect::at(320, 320).of_size(100, 100), red);
-	drawing::draw_hollow_rect_mut(&mut canvas, Rect::at(320, 320).of_size(100, 100), black);
-	draw_filled_circle_mut2(&mut canvas, (300, 300), 50, grey);
-	// draw_circle(&mut canvas.0, 300, 300, 50, blue2);
-	drawing::draw_hollow_circle_mut(&mut canvas, (300, 300), 50, black);
-	// image::save_buffer("video1.png", &canvas, 640, 640, image::ExtendedColorType::Rgba8).unwrap();
-	// let Blend(canvas) = image;
-	image::imageops::overlay(&mut canvas.0, &imgbuf1, 50, 50);
-	canvas.0.save("video1.png").unwrap();
+// 	let pixel = image::Rgba([255, 255, 255, 255]);
+// 	let mut canvas = Blend(RgbaImage::from_pixel(640, 640, pixel));// can be used to genrate a new canvas with a single color
+// 	// let mut canvas1 = image::ImageBuffer::from_pixel(640, 640, pixel);
+// 	// let mut canvas = Blend(canvas1);
+// 	let red = image::Rgba([255, 0, 0, 255]);
+// 	let blue = image::Rgba([0, 0, 255, 255]);
+// 	let black = image::Rgba([0, 0, 0, 255]);
+// 	let grey = image::Rgba([128, 128, 128, 127]);
+// 	// draw_rectangle(&mut canvas, 320, 320, 100, red);
+// 	// draw_circle(&mut canvas, 320, 320, 50, blue);
+// 	// let mut canvas = image::ImageBuffer::new(640, 640);
+// 	// let color = image::Rgb([255, 0, 0]);
+// 	// let blue = image::Rgb([0, 0, 255]);
+// 	let blue2 = image::Rgba([0, 0, 255, 127]);
+// 	drawing::draw_filled_rect_mut(&mut canvas, Rect::at(320, 320).of_size(100, 100), red);
+// 	drawing::draw_hollow_rect_mut(&mut canvas, Rect::at(320, 320).of_size(100, 100), black);
+// 	draw_filled_circle_mut2(&mut canvas, (300, 300), 50, grey);
+// 	// draw_circle(&mut canvas.0, 300, 300, 50, blue2);
+// 	drawing::draw_hollow_circle_mut(&mut canvas, (300, 300), 50, black);
+// 	// image::save_buffer("video1.png", &canvas, 640, 640, image::ExtendedColorType::Rgba8).unwrap();
+// 	// let Blend(canvas) = image;
+// 	image::imageops::overlay(&mut canvas.0, &imgbuf1, 50, 50);
+// 	canvas.0.save("video1.png").unwrap();
+// }
+
+fn random_color() -> image::Rgba<u8> {
+	let mut rng = rand::thread_rng();
+	let r = rng.gen_range(0..=255);
+	let g = rng.gen_range(0..=255);
+	let b = rng.gen_range(0..=255);
+	image::Rgba([r, g, b, 255])
+}
+
+fn main()
+{
+	let x = 80;
+	let y = 640;
+
+	let white = image::Rgba([255, 255, 255, 255]);
+	let mut canvas = RgbaImage::from_pixel(640, 640, white);
+	let rng = rand::thread_rng();
+	for j in 0..64 {
+		for i in 0..8 {
+			let color = random_color();
+			drawing::draw_filled_rect_mut(&mut canvas, Rect::at(10*i, 10*j).of_size(10, 10), color);
+			let color = random_color();
+			drawing::draw_filled_circle_mut(&mut canvas, (10*i+5, 10*j+5), 4, color);
+		}
+	}
+	//copy stripes from first generated image
+	for stripe in 1..8 {
+		for x in 0..80 {
+			for y in 0..640 {
+				let pixel = canvas.get_pixel(x, y);
+				canvas.put_pixel(x+(80*stripe), y, *pixel);
+			}
+		}
+	}
+	//apply pixel shift to generate final image
+
+	let input = image::open("42_Logo.svg.png").unwrap();
+	for stripe in 1..8 {
+		for x in 0..80 {
+			for y in 0..640 {
+				// let mut pixel = finalimg.get_pixel(x, y);
+				if y > 80 && y < 560 && (x+(80*(stripe-1)) < 480) 
+				{
+					let inputpixel = input.get_pixel(x+(80*(stripe-1)), y-80);
+					if inputpixel == image::Rgba([0, 0, 0, 255]) {
+						let mut shift = 4;
+						// if stripe > 4 {
+						// 	shift = 3;
+						// }
+						let &pixel = canvas.get_pixel(x+shift+(80*(stripe-1)), y);
+						// let &pixel2 = finalimg.get_pixel(x+(80*(stripe-1)), y);
+						for i in stripe..8 {
+							canvas.put_pixel(x+(80*i), y, pixel);
+						}
+						// finalimg.put_pixel(x+(80*(stripe+1)), y, pixel);
+						// finalimg.put_pixel(x+(80*(stripe+2)), y, pixel);
+						canvas.put_pixel(x+(80*stripe), y, pixel);
+						// pixel = &image::Rgb([255, 255, 255]);
+					}
+				}
+			}
+		}
+	}
+	image::save_buffer("video5.png", &canvas, 640, 640, image::ExtendedColorType::Rgba8).unwrap();
 }
