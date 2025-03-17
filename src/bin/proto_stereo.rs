c56f77973eb9d9aa57f51550666f494c109812b5
RoKerjea <kerjeanr@gmail.com>
2025-03-17 20:26:45 +0100

    nit:removed useless var, added comments

--- src/bin/proto_stereo.rs
+++ src/bin/proto_stereo.rs
@@ -15,7 +15,6 @@ pub fn main()
 	const MAXWIDTH : usize = 1920;
 	let xdpi = 75; //x resolution of picture to be created
 	let ydpi = 75;
-	let y_shift = ydpi/16;
 	let mut last_linked:i32;
 
 	let width=800;//width of target pic
@@ -25,12 +24,12 @@ pub fn main()
 	let mut look_r = [0;MAXWIDTH];
 	let mut vis:bool;
 	let mut color_t = vec![image::Rgba([255, 0, 0, 255]); MAXWIDTH];//store colors of those pixels, for lookup later when 2 pixels are linked
-	let obj_dist = xdpi * 12;
+	let obs_dist = xdpi * 12;//distance between the observer and the picture
 	let eye_sep = 200;
-	let max_depth = obj_dist as f32;
+	let max_depth = obs_dist as f32;
 	let max_sep = 100;
 	// eprintln!("max_sep = {}", max_sep);
-	let min_depth = (0.55 * max_depth*obj_dist as f32)/((1.0-0.55) * max_depth + obj_dist as f32);
+	let min_depth = (0.55 * max_depth*obs_dist as f32)/((1.0-0.55) * max_depth + obs_dist as f32);
 	let depth_map = image::open("assets/box.jpg").unwrap();
 	let pattern = image::open("assets/images/rect.png").unwrap();
 	let mut canvas = image::ImageBuffer::new(width as u32, height as u32);
@@ -42,7 +41,7 @@ pub fn main()
 		for x in 0..width {//link all pixels that are parts of the stereogram effect in pairs
 			let object_z =(max_depth - get_z_value(x, y, &depth_map) as f32*(max_depth-min_depth)/256.0) as u32;//get the z depth of selected pixel in depth map reference picture
 
-			let sep = ((eye_sep * object_z) / (object_z + obj_dist))as usize;
+			let sep = ((eye_sep * object_z) / (object_z + obs_dist))as usize;
 			let left : i32 = x as i32 -sep as i32/2;
 			let right = left + sep as i32;
 			vis = true;
@@ -76,9 +75,8 @@ pub fn main()
 				if last_linked == x as i32 -1 {
 					color_t[x] = color_t[x -1];
 				} else {
-					color_t[x] = pattern.get_pixel((x%100) as u32, y as u32);
+					color_t[x] = pattern.get_pixel((x%100) as u32, y as u32);//get new pixel in the pattern
 				}
-				// color_t[x] = random_dot();//only for random dot image
 			} else {
 				color_t[x] = color_t[look_l[x]];//pixel is linked with another one, so it take the same color
 				last_linked = x as i32;
@@ -100,14 +98,4 @@ pub fn get_z_value(x:usize, y:usize, depth_map:&DynamicImage) -> u32 //return a 
 	let pixel = depth_map.get_pixel(x as u32, y as u32);
 	let color = pixel.to_luma();
 	color[0] as u32
-}
-
-pub fn random_dot() -> image::Rgba<u8>{
-	let mut rng = rand::thread_rng();
-	let color = rng.gen_range(0..=1);
-	match color {
-		0 => return image::Rgba([0, 0, 0, 255]),
-		1 => return image::Rgba([255, 255, 255, 255]),
-		_ => return image::Rgba([255, 0, 0, 255]),
-	}
 }
